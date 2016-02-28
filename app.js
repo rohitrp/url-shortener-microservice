6a0bc05f80afcf1cce374dbed5561e5426d55cd7
Rohit Prasad <rohitrp3@gmail.com>
2016-02-29 00:10:24 +0530

    feat: Add methods to store data in the database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
+  , url = require('url')
   , mongo = require('mongodb').MongoClient
-  , url = "mongodb://localhost:27017/freecodecamp"
+  , mongoUrl = "mongodb://localhost:27017/freecodecamp"
   , app = express()
   , port = process.env.PORT || 3000
 
@@ -10,19 +11,81 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/index.html')
 })
 
-mongo.connect(url, function(err, db) {
-  if (err) throw err
+app.get('/new/*', function(req, res) {
+  var original_url = url.parse(req.url, true).path.replace('\/new\/', '')
   
-  var collection = db.collection('urls')
-  
-  collection.insert({
-    original_url: '',
-    short_url: ''
-  }, function(err) {
+  mongo.connect(mongoUrl, function(err, db) {
     if (err) throw err
-    db.close()
+
+    var collection = db.collection('urls')
+    
+    collection.findOneAndUpdate(
+      { _id: 'urlId' },
+      { $inc: { sequence_value: 1 }},
+      {
+        upsert: true,
+        returnNewDocument: true
+      },
+      function(err, updated) {
+
+        collection.insert({
+          
+          _id: updated.value.sequence_value,
+          original_url: original_url,
+          short_url: idToShortUrl(updated.value.sequence_value)
+          
+        }, function(err) {
+          if (err) throw err
+          db.close()
+        })
+      }
+    )
+  })
+
+})
+
+function idToShortUrl(id) {
+  var map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    , shortUrl = []
+  
+  while (id) {
+    shortUrl.unshift(map.charAt(id % 62))
+    id = Math.floor(id / 62)
+  }
+  
+  return shortUrl.join('')
+}
+
+// Route to initialize the sequence counter
+app.get('/init', function(req, res) {
+  mongo.connect(mongoUrl, function(err, db) {
+    db.collection('urls').insert({
+      _id: 'urlId',
+      sequence_value: 0
+    }, function(err) {
+      if (err) throw err
+      db.close()
+    })
   })
 })
+
+function shortUrltoId(original_url) {
+  var id = 0
+  
+  for (var i = 0; i < original_url.length; i++) {
+    var ch = original_url.charAt(i)
+    
+    if ('a' <= ch && ch <= 'z') {
+      id = id * 62 + ch.charCodeAt(0) - 'a'.charCodeAt(0)
+    } else if ('A' <= ch && ch <= 'Z') {
+      id = id * 62 + ch.charCodeAt(0) - 'A'.charCodeAt(0) + 26
+    } else if ('0' <= ch && ch <= '9') { 
+      id = id * 62 + ch - '0'
+    }
+  }
+  
+  return id
+}
 
 app.listen(port, function(err) {
   console.log('Listening at port ' + port + '...')
